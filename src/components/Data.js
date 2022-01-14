41b820defdacc8544ec5745655bce4c9a36e6fd5
bilal <bilal.gkdg@gmail.com>
2022-01-14 12:44:22 +0300

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+export const Data = [
+  {
+    question: 'What do you call a dog magician?',
+    answer: 'A labracadabrador.'
+  },
+  {
+    question: 'What do you call a dog magician?',
+    answer: 'A labracadabrador.'
+  },
+  {
+    question: 'What do you call a dog magician?',
+    answer: 'A labracadabrador.'
+  }
+];
